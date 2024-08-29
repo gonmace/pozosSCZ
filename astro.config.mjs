b091f2351498a482f9013db701c008d15463a93b
gonmace <magoreal38@gmail.com>
2024-08-28 20:17:10 -0400

    static

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,14 @@
 import { defineConfig } from 'astro/config';
 import icon from "astro-icon";
 import tailwind from "@astrojs/tailwind";
-import node from "@astrojs/node";
+
 
 import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [icon(), tailwind(), sitemap()],
-  output: 'server',
+  output: 'static',
   site: 'https://pozosscz.com',
-  adapter: node({
-    mode: "standalone"
-  })
+
 });
--- src/data/menuData.ts
+++ src/data/menuData.ts
@@ -5,7 +5,7 @@ const menuData: MenuItem[] = [
   { name: "Inicio", link: "/", icon: "menu/home", protected: false},
   { name: "Cotiza", link: "/cotiza", icon: "menu/pesos", protected: false },
   { name: "Contáctanos", link: "/contacto", icon: "menu/contact", protected: false },
-  { name: "Mapa", link: "/mapa", icon: "menu/mapa", protected: true },
+  // { name: "Mapa", link: "/mapa", icon: "menu/mapa", protected: true },
 ];
 
 export default menuData;
